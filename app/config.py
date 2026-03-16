517afb5d2fc74eb7e58c00cc32bade6066b7fa68
David Odimayo <odimayodavid7@gmail.com>
2026-03-16 19:29:59 +0100

    fix(config): Pydantic mismatch

--- app/config.py
+++ app/config.py
@@ -1,40 +1,34 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class PostgresSettings(BaseSettings):
     DB: str
     USER: str
     PASSWORD: str
-
-    model_config = SettingsConfigDict(env_prefix='POSTGRES_')
+    model_config = SettingsConfigDict(env_prefix='POSTGRES_', env_file=BASE_DIR / ".env" , extra="ignore")
 
 
 class JWTSettings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-
-    model_config = SettingsConfigDict(env_prefix="JWT_")
+    model_config = SettingsConfigDict(env_prefix="JWT_", env_file=BASE_DIR / ".env" , extra="ignore")
 
 
 class CelerySettings(BaseSettings):
     BROKER_URL: str
     RESULT_BACKEND: str
-
-    model_config = SettingsConfigDict(env_prefix="CELERY_")
+    model_config = SettingsConfigDict(env_prefix="CELERY_", env_file=BASE_DIR / ".env" , extra="ignore")
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    postgres: PostgresSettings = Field(default_factory=PostgresSettings)
-    jwt: JWTSettings = Field(default_factory=JWTSettings)
-    celery: CelerySettings = Field(default_factory=CelerySettings)
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8"
-    )
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", env_file_encoding="utf-8" , extra="ignore")
 
 
 settings = Settings()
+postgres_settings = PostgresSettings()
+jwt_settings = JWTSettings()
+celery_settings = CelerySettings()
