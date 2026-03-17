13e45f36074b5a629de129b33ed67b285ed35ab5
David Odimayo <odimayodavid7@gmail.com>
2026-03-17 16:47:35 +0100

    feat(user-router): Implementation of user routes

---
+++ app/users/router.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends
+from app.dtos.user_dto import TokenRequest, TokenResponse, UserCreateRequest, UserResponse, UserUpdateRequest
+from app.auth.dependencies import get_current_user, requires_admin
+from app.users.service import UsersService
+from app.users.models import Users
+from typing import Optional
+from app.users.dependencies import get_user_service
+
+router = APIRouter(prefix="/api/auth", tags=["Users"])
+
+@router.post("/token", response_model=TokenResponse)
+def login(
+    payload: TokenRequest,
+    user_service: UsersService = Depends(get_user_service)
+    ):
+    return user_service.login_user(payload)
+
+@router.post("/register", response_model=UserResponse, status_code=201)
+def register(
+    payload: UserCreateRequest,
+    user_service: UsersService = Depends(get_user_service)
+    ):
+    return user_service.register_user(payload)
+
+@router.get("/users", response_model=list[UserResponse])
+def get_users(
+    active: Optional[bool] = None,
+    current_user: Users = Depends(requires_admin),
+    user_service: UsersService = Depends(get_user_service)
+    ):
+    return user_service.get_users(current_user, active=active)
+
+@router.get("/users/{id}", response_model=UserResponse)
+def get_user(
+    id: int,
+    current_user: Users = Depends(get_current_user),
+    user_service: UsersService = Depends(get_user_service)
+    ):
+    return user_service.get_user(id, current_user)
+
+@router.put("/users/{id}", response_model=UserResponse)
+def update_user(
+    id: int,
+    payload: UserUpdateRequest,
+    current_user: Users = Depends(requires_admin),
+    user_service: UsersService = Depends(get_user_service)):
+    
+    return user_service.update_user(id, payload, current_user)
+
+#will be implemented with the AccountClosureService
+@router.post("/users/{id}/archive")
+def archive_user(
+    id: int,
+    current_user: Users = Depends(get_current_user),
+    user_service: UsersService = Depends(get_user_service)):
+    pass
+
