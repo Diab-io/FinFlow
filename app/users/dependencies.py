6420bac7ad73f78c01b805d482c4cbce264fb27b
David Odimayo <odimayodavid7@gmail.com>
2026-03-17 16:43:37 +0100

    feat(user-dep): Added dependencies for the user domain

---
+++ app/users/dependencies.py
@@ -0,0 +1,7 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.users.service import UsersService
+
+def get_user_service(db: Session = Depends(get_db)) -> UsersService:
+    return UsersService(db)
