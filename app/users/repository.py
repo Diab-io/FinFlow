ce22f0e59c5a99bbb4b25e005e2c1e65413fa052
David Odimayo <odimayodavid7@gmail.com>
2026-03-17 16:46:50 +0100

    refactor(user-repo): Removed redundant filter method, moved to base repo

--- app/users/repository.py
+++ app/users/repository.py
@@ -8,20 +8,3 @@ class UsersRepository(BaseRepository[Users]):
     def __init__(self, db: Session):
         super().__init__(Users, db)
     
-    def get_user(self, **filters):
-        ALLOWED_FIELDS = {"email", "username", "phone"}
-
-        conditions = []
-
-        for field, value in filters.items():
-            if field in ALLOWED_FIELDS and value is not None:
-                column = getattr(Users, field, None)
-                if column is not None:
-                    conditions.append(column == value)
-        
-        if not conditions:
-            return None
-        
-        query = select(Users).where(and_(**conditions))
-        return self.db.execute(query).scalar_one_or_none()
-
