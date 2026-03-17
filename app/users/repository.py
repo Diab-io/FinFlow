ab427d67bb5e83791b2fe55f255d2cf8d117c457
David Odimayo <odimayodavid7@gmail.com>
2026-03-17 17:38:38 +0100

    feat(users-repo): Implemented search by email

--- app/users/repository.py
+++ app/users/repository.py
@@ -1,10 +1,16 @@
 from app.base_repo import BaseRepository
 from app.users.models import Users
 from sqlalchemy.orm import Session
-from sqlalchemy import select, and_
+from sqlalchemy import select
 
 
 class UsersRepository(BaseRepository[Users]):
+    ALLOWED_SEARCH_FIELDS = {"active", "email", "username", "phone"}
+
     def __init__(self, db: Session):
         super().__init__(Users, db)
+    
+    def get_user_by_email(self, email):
+        stmt = select(Users).where(Users.email == email)
+        return self.db.execute(stmt).scalar_one_or_none()
     
