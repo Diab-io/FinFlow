3b47dadc9e9cfa642ab614720ce419820c386e4e
David Odimayo <odimayodavid7@gmail.com>
2026-03-19 19:32:59 +0100

    feat(wallet-repo): Implemented methods to get wallet information

---
+++ app/wallets/repository.py
@@ -0,0 +1,51 @@
+from app.base_repo import BaseRepository
+from app.wallets.models import Wallets
+from app.transactions.models import Transactions
+from app.enums import TransactionTypeEnum
+from uuid import UUID
+from sqlalchemy import select, case, func, and_
+from sqlalchemy.orm import Session
+
+
+class WalletRepository(BaseRepository[Wallets]):
+    def __init__(self, db: Session):
+        super().__init__(Wallets, db)
+
+    def get_wallet_by_acc_number(self, account_number: str) -> Wallets:
+        query = select(Wallets).where(Wallets.account_number == account_number)
+        wallet = self.db.execute(query).scalar_one_or_none()
+        return wallet
+
+    def get_wallet_by_user_id(self, user_id: UUID):
+        query = select(Wallets).where(Wallets.user_id == user_id)
+        wallet = self.db.execute(query).scalar_one_or_none()
+        return wallet
+
+    def get_wallet_balance(self, wallet_id: UUID):
+        credits = self.db.execute(
+            func.coalesce(
+                func.sum(
+                    select(Transactions).where(
+                        and_(Transactions.wallet_id == wallet_id, Transactions.type == TransactionTypeEnum.CREDIT)
+                    )
+                ), 0
+            )
+        )
+
+        debits = self.db.execute(
+            func.coalesce(
+                func.sum(
+                    select(Transactions).where(
+                        and_(Transactions.wallet_id == wallet_id, Transactions.type == TransactionTypeEnum.DEBIT)
+                    )
+                ), 0
+            )
+        )
+
+        balance = credits - debits
+        return balance
+    
+    def get_wallet_transactions(self, wallet_id: UUID):
+        query = select(Transactions).where(Transactions.wallet_id == wallet_id)
+        transactions = self.db.execute(query).all()
+        return transactions
