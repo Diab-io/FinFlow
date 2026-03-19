fc1d7962414b7a28157af219c470f8e848dde2e5
David Odimayo <odimayodavid7@gmail.com>
2026-03-19 17:23:49 +0100

    feat(wallet-model): Created model for wallets

---
+++ app/wallets/models.py
@@ -0,0 +1,18 @@
+from app.database import Base
+from app.enums import CurrencyEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String
+from uuid import uuid4, UUID
+from datetime import datetime, timezone
+
+
+class Wallets(Base):
+    __tablename__ = 'wallets'
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, index=True, default=uuid4)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'), unique=True)
+    account_number: Mapped[str] = mapped_column(String(10), index=True, unique=True)
+    currency: Mapped[CurrencyEnum] = mapped_column(default="NGN")
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
+
+    transactions: Mapped[list["Transactions"]] = relationship(back_populates="wallet")
