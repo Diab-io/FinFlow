7e17279b91e595123e952c99140435fe1e253fbb
David Odimayo <odimayodavid7@gmail.com>
2026-03-21 16:36:04 +0100

    chore(main.py): Included wallet router to the main router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.users.router import router as user_route
+from app.wallets.router import router as wallet_route
 
 app = FastAPI()
 app.include_router(user_route)
+app.include_router(wallet_route)
