a6dc372e23b776c78d97f3a1a022b140b5c70c14
David Odimayo <odimayodavid7@gmail.com>
2026-03-21 16:32:26 +0100

    chore(wallet-dep): Adapted core folder

--- app/wallets/dependencies.py
+++ app/wallets/dependencies.py
@@ -1,6 +1,6 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from app.database import get_db
+from app.core.database import get_db
 from app.wallets.service import WalletService
 
 def get_wallet_service(db: Session = Depends(get_db)) -> WalletService:
