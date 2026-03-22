96fb77eeb4f92dd4d8fd9289882cc43a978cf2d0
David Odimayo <odimayodavid7@gmail.com>
2026-03-22 23:20:42 +0100

    feat(routes): Added transfer route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.users.router import router as user_route
 from app.wallets.router import router as wallet_route
+from app.transfers.router import router as transfer_route
 
 app = FastAPI()
 app.include_router(user_route)
 app.include_router(wallet_route)
+app.include_router(transfer_route)
