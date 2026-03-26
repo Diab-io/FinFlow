54d2b01e3eba7f7db567ea7ef099fe640c110dbe
David Odimayo <odimayodavid7@gmail.com>
2026-03-26 21:03:37 +0100

    chore: Hooked payment route to the main router

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from app.users.router import router as user_route
 from app.wallets.router import router as wallet_route
 from app.transfers.router import router as transfer_route
+from app.payments.router import router as payment_route
 
 app = FastAPI()
 app.include_router(user_route)
 app.include_router(wallet_route)
 app.include_router(transfer_route)
+app.include_router(payment_route)
