54b903d407069c2a27af3d52faf6a99ad7e5480c
David Odimayo <odimayodavid7@gmail.com>
2026-03-31 14:42:50 +0100

    feat: Added shared dependencies for tests

---
+++ tests/conftest.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+from dotenv import load_dotenv
+load_dotenv()
+from app.main import app
+from app.core.database import Base, get_db
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+TEST_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
+engine = create_engine(TEST_DATABASE_URI)
+TestSession = sessionmaker(bind=engine)
+
+
+@pytest.fixture
+def db():
+    Base.metadata.create_all(bind=engine)
+    session = TestSession
+    yield session
+    session.close()
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def client(db):
+    def get_override_db():
+        yield db
+    
+    app.dependency_overrides[get_db] = get_override_db
+    yield TestClient(app)
+    app.dependency_overrides.clear()
